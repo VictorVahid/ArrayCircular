f2965af4e6db73187adfa375c2d48a719936f66d
Victor Vahid <victorvahid76@gmail.com>
2026-09-11 12:57:20 -0400

    Criação do Modulus para exemplificar visualmente a Aritmética modular

--- script.js
+++ script.js
@@ -3,21 +3,17 @@ const total = cards.length;
 let current = 0;
 
 function renderizar() {
-  // Limpa a classe de todos os cards deixando apenas o padrão
   cards.forEach((card) => (card.className = "card"));
 
-  // Aplica as fórmulas matemáticas do array circular
   let idxAtual = current;
   let idxFrente = (current + 1) % total;
   let idxTras = (current - 1 + total) % total;
 
-  // Distribui as classes baseando-se no cálculo do índice
   cards[idxAtual].classList.add("active");
   cards[idxFrente].classList.add("neighbor");
   cards[idxTras].classList.add("neighbor");
 }
 
-// Ouvintes de clique isolados e definitivos
 document.getElementById("next").addEventListener("click", () => {
   current = (current + 1) % total;
   renderizar();
@@ -28,5 +24,4 @@ document.getElementById("prev").addEventListener("click", () => {
   renderizar();
 });
 
-// Executa a primeira renderização na tela
 renderizar();
